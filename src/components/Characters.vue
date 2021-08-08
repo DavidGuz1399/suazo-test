94054eeb20c24201d9a72d9cf5d328e83c4bc64f
David Guzman <david.guzman@itglobers.com>
2021-08-08 15:58:18 -0500

    Favorito funcional

---
+++ src/components/Characters.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div
+      v-for="(character, index) in characters"
+      v-bind:key="index"
+    >
+      <div>
+        <img :src="character.image" alt="" />
+      </div>
+      <div>
+        <span>{{ character.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((response) => {
+        console.log(response);
+        this.characters = response.data.results;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
+</script>
